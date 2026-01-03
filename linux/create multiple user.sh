282a9c460eb547ef9a8cc56a04a37368ba6f6eee
kumsa Mergia <kumsamergia@gmail.com>
2026-01-03 12:29:15 +0300

    Create Multiple User

---
+++ linux/create multiple user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERS="naole kaleb zerihune"
+PASSWORD="ToSp#54321"
+
+# Create users with home directories and bash shell
+for user in $USERS; do
+    /usr/sbin/useradd -m -s /bin/bash "$user"
+done
+
+# Set password for each user
+for user in $USERS; do
+    echo "$user:$PASSWORD" | chpasswd
+done
+
+# Force password change at first login
+for user in $USERS; do
+    passwd -e "$user"
+done
+
+# Add users to sudo group
+for user in $USERS; do
+    /usr/sbin/usermod -aG sudo "$user"
+done
