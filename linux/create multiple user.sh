db3333009fdb8e7eb1d4ef74161c8ec9ba68d86f
kumsa Mergia <kumsamergia@gmail.com>
2026-01-05 15:14:31 +0300

    Remove hardcoded secret

--- linux/create multiple user.sh
+++ linux/create multiple user.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 USERS="naole kaleb zerihune"
+PASSWORD="${USER_PASSWORD:?Password not set}"
 
-# Prompt for password securely (no echo)
-read -s -p "Enter password for new users: " PASSWORD
-echo
-read -s -p "Confirm password: " CONFIRM
-echo
-
-if [ "$PASSWORD" != "$CONFIRM" ]; then
-    echo "Passwords do not match"
-    exit 1
-fi
-
+# Create users with home directories and bash shell
 for user in $USERS; do
     /usr/sbin/useradd -m -s /bin/bash "$user"
+done
+
+# Set password for each user
+for user in $USERS; do
     echo "$user:$PASSWORD" | chpasswd
+done
+
+# Force password change at first login
+for user in $USERS; do
     passwd -e "$user"
+done
+
+# Add users to sudo group
+for user in $USERS; do
     /usr/sbin/usermod -aG sudo "$user"
 done
